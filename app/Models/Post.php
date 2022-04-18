3860fdcae851a166a27e4b3ec6ac6d9594df9781
vencelg <gazdavaclav@gmail.com>
2022-04-18 10:27:55 +0200

    Started working on Comments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,7 +16,19 @@ class Post extends Model
         'user_id',
     ];
 
-    public function user() {
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
--- database/migrations/2022_04_18_073859_create_comments_table.php
+++ database/migrations/2022_04_18_073859_create_comments_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -12,13 +13,12 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('header');
-            $table->string('body');
-            $table->string('image')->nullable();
-            $table->integer('upvotes');
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->string('content');
+            $table->integer('upvotes');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
