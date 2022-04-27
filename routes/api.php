18fdf2ace9c7006cba3c0ca18c6e925067862c83
vencelg <gazdavaclav@gmail.com>
2022-04-27 10:26:26 +0200

    Added Cors Middleware

--- routes/api.php
+++ routes/api.php
@@ -14,32 +14,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'auth'], function () {
-    Route::get('', [\App\Http\Controllers\AuthController::class, 'index'])->middleware("auth:sanctum");
-    Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
-    Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
-    Route::get('logout', [\App\Http\Controllers\AuthController::class, 'logout'])->middleware("auth:sanctum");
-    Route::post('userProfile', [\App\Http\Controllers\AuthController::class, 'userProfile'])->middleware("auth:sanctum");
-    Route::post('editUser', [\App\Http\Controllers\AuthController::class, 'editUser'])->middleware("auth:sanctum");
-});
+Route::group(['middleware' => 'cors'], function () {
+    Route::group(['prefix' => 'auth'], function () {
+        Route::get('', [\App\Http\Controllers\AuthController::class, 'index'])->middleware("auth:sanctum");
+        Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+        Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+        Route::get('logout', [\App\Http\Controllers\AuthController::class, 'logout'])->middleware("auth:sanctum");
+        Route::post('userProfile', [\App\Http\Controllers\AuthController::class, 'userProfile'])->middleware("auth:sanctum");
+        Route::post('editUser', [\App\Http\Controllers\AuthController::class, 'editUser'])->middleware("auth:sanctum");
+    });
 
-Route::group(['prefix' => 'verify', 'middleware' => 'auth:sanctum'], function () {
-    Route::post('resend', [\App\Http\Controllers\EmailVerificationController::class, 'resend']);
-    Route::get('verify-email/{id}/{hash}', [\App\Http\Controllers\EmailVerificationController::class, 'verify'])->name('verification.verify');
-});
+    Route::group(['prefix' => 'verify', 'middleware' => 'auth:sanctum'], function () {
+        Route::post('resend', [\App\Http\Controllers\EmailVerificationController::class, 'resend']);
+        Route::get('verify-email/{id}/{hash}', [\App\Http\Controllers\EmailVerificationController::class, 'verify'])->name('verification.verify');
+    });
 
-Route::group(['middleware' => ['auth:sanctum', 'isVerified']], function () {
+    Route::group(['middleware' => ['auth:sanctum', 'isVerified']], function () {
 
-    Route::post('friends', [\App\Http\Controllers\FriendController::class, 'store']);
-    Route::put('friends/{id}', [\App\Http\Controllers\FriendController::class, 'update']);
-    Route::delete('friends/{id}', [\App\Http\Controllers\FriendController::class, 'destroy']);
+        Route::post('friends', [\App\Http\Controllers\FriendController::class, 'store']);
+        Route::put('friends/{id}', [\App\Http\Controllers\FriendController::class, 'update']);
+        Route::delete('friends/{id}', [\App\Http\Controllers\FriendController::class, 'destroy']);
 
-    Route::apiResources([
-        'posts' => \App\Http\Controllers\PostController::class,
-        'comments' => \App\Http\Controllers\CommentController::class,
-        'responses' => \App\Http\Controllers\ResponseController::class,
-    ]);
+        Route::apiResources([
+            'posts' => \App\Http\Controllers\PostController::class,
+            'comments' => \App\Http\Controllers\CommentController::class,
+            'responses' => \App\Http\Controllers\ResponseController::class,
+        ]);
 
-    Route::get('users/{searchKey}', [\App\Http\Controllers\SearchController::class, 'search']);
+        Route::get('users/{searchKey}', [\App\Http\Controllers\SearchController::class, 'search']);
+    });
 });
 
